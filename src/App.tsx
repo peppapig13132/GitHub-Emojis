62c4d463bbf9866b30ec10ab19b569d00cd06cb4
peppapig13132 <peppapig13132@outlook.com>
2024-06-13 14:02:56 +0200

    Create component: Heroicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import "./App.css";
+import './App.css';
 import { getEmojis } from './services/githubApiService';
 import { Emoji } from './components/emoji/Emoji';
 import { EmojiData } from './interfaces/interfaces';
@@ -21,17 +21,17 @@ function App() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center min-h-screen">
-      <h2 className="text-4xl mt-12 mb-8 text-slate-700">
+    <div className='flex flex-col items-center min-h-screen'>
+      <h2 className='text-4xl mt-12 mb-8 text-slate-700'>
         GitHub Emojis
       </h2>
 
-      <div className="flex flex-wrap w-full justify-items-center">
+      <div className='flex flex-wrap w-full justify-items-center'>
         {
           emojis.map((emoji, key) => <Emoji name={emoji.name} link={emoji.link} width={50} height={50} key={key} />)
         }
       </div>
-      <p className="my-8 text-slate-700">Made with ❤️ by <a target="_blank" href="https://github.com/peppapig13132">Pepp</a></p>
+      <p className='my-8 text-slate-700'>Made with ❤️ by <a target='_blank' rel='noreferrer' href='https://github.com/peppapig13132'>Pepp</a></p>
     </div>
   );
 }
